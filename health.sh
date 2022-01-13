b158b5881a6fd67e2d0392f26f189466c0f661f3
David Lantos <david.lantos@gmail.com>
2022-01-13 10:52:58 +0000

    Display which env var the URL came from in health.sh
    This helps with not having to look up the value when it's wrong or down

--- health.sh
+++ health.sh
@@ -7,12 +7,17 @@ function try_configured_urls() {
   local extracted_urls
   extracted_urls="$(kubectl get "deployment/$deployment" --namespace="$namespace" \
     -o=jsonpath='{.spec.template.spec.containers[].env}' | \
-    jq -r '.[] | select(.name | endswith("URL")) | .value + "/health" | select(contains(".gov.uk") or contains(".dsd.io"))')"
+    jq -r '.[] | select(.name | endswith("URL")) | select(.value | contains(".gov.uk") or contains(".dsd.io")) |
+      .name + "," + .value + "/health"')"
 
-  for url in $extracted_urls; do
+  for kv in $extracted_urls; do
+    IFS=',' read -r -a elems <<< "$kv"
+    env_var="${elems[0]}"
+    url="${elems[1]}"
+
     printf "%-40s" "$deployment"
     printf "%-40s" "on $namespace"
-    printf "%s" "using $url: "
+    printf "found %-40s checking %s: " "$env_var" "$url"
     check="⛔️ down"
     if curl "$url" --max-time 5 --silent -o/dev/null; then
       check="✅ up"
