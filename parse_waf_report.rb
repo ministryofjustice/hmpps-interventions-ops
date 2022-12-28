a339f4a68e97068f7d421e6f95dd8561b7d1e7ce
David Lantos <david.lantos@gmail.com>
2022-12-28 12:10:34 +0000

    Fix static analysis (rubocop) warnings on WAF parser

--- parse_waf_report.rb
+++ parse_waf_report.rb
@@ -10,29 +10,29 @@ NUMERIC_REGEX = /\b[0-9]+\b/
 # set time period from 2022-12-05 10:00 UTC for every POST
 # use "Reporting" > "Download CSV"
 csv = CSV
-  .parse(STDIN, headers: true)
-  .reject { |r| r["log_processed.transaction.messages"] == '[]' }
+      .parse(STDIN, headers: true)
+      .reject { |r| r['log_processed.transaction.messages'] == '[]' }
 
 errors = []
 
 csv.each do |r|
   error = {}
-  m = JSON.parse(r["log_processed.transaction.messages"])
-  error["kind"] = m.first["message"]
-  error["tags"] = m.first["details"]["tags"].sort
+  m = JSON.parse(r['log_processed.transaction.messages'])
+  error['kind'] = m.first['message']
+  error['tags'] = m.first['details']['tags'].sort
 
-  req = r["log_processed.transaction.request.uri"]
-    .gsub(UUID_REGEX, '{uuid}')
-    .gsub(NUMERIC_REGEX, '{number}')
-  error["request"] = req
+  req = r['log_processed.transaction.request.uri']
+        .gsub(UUID_REGEX, '{uuid}')
+        .gsub(NUMERIC_REGEX, '{number}')
+  error['request'] = req
 
   errors << error
 end
 
-errors.group_by { |e| e["request"] }.each do |request, problems|
+errors.group_by { |e| e['request'] }.each do |request, problems|
   puts
-  puts "#{request}:"
-  problems.tally { |p| p["kind"] }.each do |p,c|
-    puts "  #{c} x #{p["kind"]}"
+  puts '#{request}:'
+  problems.tally { |p| p['kind'] }.each do |p,c|
+    puts "  #{c} x #{p['kind']}"
   end
 end
